d5a73067beebd9e559daa8058e99120565285f44
Orik <orxan.aliev.2000@gmail.com>
2021-12-14 10:40:11 +0400

    14.12.21

---
+++ moi proekti/moi proekti/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace moi_proekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vedite rostoyanie");
+            Double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("vedite kurs dollara");
+            Double kurs = Convert.ToDouble(Console.ReadLine());
+            Double prise = r * 1.5 * kurs;
+            Console.WriteLine($"pri rostoyanie {r},pri kurse {kurs},stoimost poezdki={prise}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
